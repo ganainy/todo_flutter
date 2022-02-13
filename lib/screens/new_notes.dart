7005ac1398c34ac14d24fbe36c07a073355519e3
Amr <afoda50@gmail.com>
2022-02-13 23:32:29 +0100

    show list of saved notes with bloc

--- lib/screens/new_notes.dart
+++ lib/screens/new_notes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_flutter/note_bloc/note_cubit.dart';
+import 'package:todo_flutter/shared/reusable_components.dart';
 
 class NewNotes extends StatefulWidget {
   const NewNotes({Key? key}) : super(key: key);
@@ -10,13 +13,26 @@ class NewNotes extends StatefulWidget {
 class _NewNotesState extends State<NewNotes> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'New Notes',
-          style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-        ),
-      ),
+    var noteCubit = NoteCubit.get(context);
+    //TODO: zbt amaken astd3a2 al initiatedb w getnotes
+    noteCubit.getNotes();
+    return BlocConsumer<NoteCubit, NoteStates>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Container(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return NoteItem(
+                  note: noteCubit.noteList.elementAt(index)['content'],
+                  time: noteCubit.noteList.elementAt(index)['time'],
+                  date: noteCubit.noteList.elementAt(index)['date']);
+            },
+            itemCount: noteCubit.noteList.length,
+          ),
+        );
+      },
     );
   }
 }
